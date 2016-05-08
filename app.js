9c0fa4d119d0a6ec974b483b5d6d606608f3f2a8
Amy Etheredge <amy.etheredge@gmail.com>
2016-05-07 20:04:43 -0400

    floundering around a little more

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ var hanoi = [
   []
 ];
 
-function move (source, target) {
-  var value = hanoi[source].pop();
-  hanoi[target].push(value);
+var move = function (tower, source, target) {
+
+  source = source | $('#source').val();
+  target = target | $('#target').val();
+  tower = tower || hanoi;
+
+  if (source > 2 || source < 0 || target > 2 || target < 0) {
+    return false;
+  }
+
+  var value = tower[source].pop();
+  tower[target].push(value);
+
+  $('#source').val('');
+  $('#target').val('');
 }
 
+function setTowers () {
+  $('#pile_1').html(hanoi[0]);
+  $('#pile_2').html(hanoi[1]);
+  $('#pile_3').html(hanoi[2]);
+}
 
-//document.getElementById('pile1').innerHTML = hanoi[0];
+$(document).ready(function(){
+	
+  setTowers();
 
-// if (hanoi[1] !== null) {
-//   document.getElementById('pile2').innerHTML = hanoi[1];
-// }
-// if (hanoi[2] !== null) {
-//   document.getElementById('pile2').innerHTML = hanoi[2];
-// }
+  $('#move').on('click', function() {
+    move();
+    setTowers();
+  });
+
+});
--- test.js
+++ test.js
@@ -23,3 +23,61 @@ QUnit.test('Ensure no piece in each pile is underneath a bigger piece', function
   });
 });
 
+QUnit.test('Move function should take a piece off the top of source pile', function (assert) {
+  var tower = [
+    [5,4,3,2,1],
+    [],
+    []
+  ];
+  var currPile = tower[0].length
+  move(tower, 0,1);
+  assert.strictEqual(tower[0].length === (currPile - 1), true );
+});
+
+QUnit.test('Move function should place a piece on the target pile', function (assert) {
+  var tower = [
+    [5,4,3,2,1],
+    [],
+    []
+  ];
+  var targetPile = tower[1].length
+  move(tower,0,1);
+  assert.strictEqual(tower[1].length === (targetPile + 1), true );
+});
+
+QUnit.test('Move should have both source and target defined', function (assert) {
+  assert.throws(move(target))
+  assert.throws(move(target, 0))
+});
+
+QUnit.test('Source/Target should be restricted to pile 1, 2, or 3', function (assert) {
+  var tower = [
+    [5,4,3,2,1],
+    [],
+    []
+  ];
+  assert.strictEqual(move(tower, 4,1), false);
+  assert.strictEqual(move(tower, -1,1), false);
+  assert.strictEqual(move(tower, 1,4), false);
+  assert.strictEqual(move(tower, 1,-1), false);
+});
+  
+QUnit.test('After move, target/source values should be blank', function(assert) {
+  var tower = [
+    [5,4,3,2,1],
+    [],
+    []
+  ];
+  move(tower,0,1);
+  assert.strictEqual($('#target').val() == '', true);
+  assert.strictEqual($('#source').val() == '', true);
+});
+
+QUnit.test('Towers on page should represent hanoi array', function(assert) {
+  $(document).ready(function(){
+    assert.strictEqual($('#pile_1').html() === hanoi[0].join(''), true);
+    assert.strictEqual($('#pile_2').html() === hanoi[1].join(''), true);
+    assert.strictEqual($('#pile_3').html() === hanoi[2].join(''), true);
+  });
+
+});
